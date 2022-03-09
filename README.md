1e3095734b849645eb364d1e54c16641b0aee594
Liam Brannigan <l.brannigan@analyticsengines.com>
2022-03-09 16:30:48 +0000

    update main

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # performance-data-science
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/braaannigan/performance-data-science/HEAD)
---
+++ arrays/test_numpyParallelSimple.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from testbook import testbook
+import numpy as np
+
+notebookPath = Path("arrays/numpyParallelSimple.ipynb")
+
+
+@testbook(notebookPath.as_posix(), execute=True)
+def test_func(tb):
+    generateData = tb.get("generateData")
+    xyLength = 3
+    timesteps = 3
+    output = generateData(xyLength=xyLength, timesteps=timesteps)
+    assert isinstance(output, np.ndarray)
